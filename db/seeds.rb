d0f8f6579269833665ff7e1fc67b54f4a0e6d718
bo <bobppamail@gmail.com>
2019-03-04 17:26:40 +0100

    fixed all the errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,27 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-  while Cleaners.count < 40
-    Cleaners.create! name:Faker::Name.female_first_name, description:Faker::Quote.famous_last_words, price: rand(7..20), age: rand(18..70), average_rating: rand(0..5)
-  end
+while Cleaner.count < 40
+  Cleaner.create! name:Faker::Name.female_first_name, description:Faker::Quote.famous_last_words, price: rand(7..20), age: rand(18..70), average_rating: rand(0..5)
+end
 
-  while Users.count < 10
-    Users.create! email: Faker::Internet.email, encrypted_password: 'password1234', address: Faker::Address.full_address
-  end
+while User.count < 20
+  User.create! email: Faker::Internet.email, encrypted_password: 'password1234', address: Faker::Address.full_address, password: 'thisisapass1234'
+end
 
-  while Reviews.count < 15
-    Reviews.create! rating:rand(1..5), content:Faker::Movies::HarryPotter.quote
-  end
+guard = 1
+while Reservation.count < 9
+  my_res = Reservation.new
+  my_res.user = User.find(guard)
+  my_res.cleaner = Cleaner.find(guard)
+  guard += 1
+  my_res.save
+end
 
-  i = 1
-  while Reservations.count < 9
-    my_res = Reservations.new
-    my_res.users = Users.find(i)
-    my_res.cleaners = Cleaners.find(i)
-    i += 1
-  end
+while Review.count < 15
+  rev = Review.new
+  rev.rating = rand(1..5)
+  rev.content = Faker::Movies::HarryPotter.quote
+  rev.reservation = Reservation.find(rand(1..9))
+  rev.save
+end

41e0ab5cc3fddcb5106a3f0a56fa780e980b4a11
Rodolphe5537 <rodolphe@dedorlodot.net>
2019-03-04 17:43:21 +0100

    reservation controller + validations and resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
+  resources :cleaners, only: [:index, :show]
+  resources :reservations
   devise_for :users
-  root to: 'pages#home'
+  root to: 'cleaners#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

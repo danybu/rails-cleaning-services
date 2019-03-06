6584471bbf8e3fc5ded91f542e1d399c8921db87
bo <bobppamail@gmail.com>
2019-03-06 12:07:09 +0100

    added rating on my show page NEED TO BE FIXED

--- app/controllers/cleaners_controller.rb
+++ app/controllers/cleaners_controller.rb
@@ -20,10 +20,32 @@ class CleanersController < ApplicationController
   end
 
   def show
-    @reviews = []
     @cleaner = Cleaner.find(params[:id])
+      rating_sum = 0.0
+      rating_count = 0
+      @cleaner.reservations.each do |reservation|
+        if !reservation.reviews.empty?
+          rating_sum += reservation.reviews[0].rating
+          rating_count += 1
+        end
+      if rating_sum == 0
+        @cleaner.average_rating = 0
+      else
+        @cleaner.average_rating = rating_sum / rating_count
+      end
+
+    end
+
+    @reviews = []
     reservations = Reservation.where(cleaner_id: params[:id], status:'2') #get all finish rsv of current cleaner
     reservations.each { |rsv| @reviews << Review.find(rsv.id).content  }
     @booking = Reservation.new
+  end
+
+
+  private
+
+  def average_calcul cleaner
+
   end
 end

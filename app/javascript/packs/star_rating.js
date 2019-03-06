025a948846fcd84c295af5e83aa9fd6c0eae8c26
bo <bobppamail@gmail.com>
2019-03-07 00:18:09 +0100

    just cleaning above code

--- app/javascript/packs/star_rating.js
+++ app/javascript/packs/star_rating.js
@@ -1,10 +1,12 @@
 const star_rating = () => {
-  console.log('INSIDE STAR')
   const starTotal = 5;
-  const ratingOnPage = document.getElementById('rating').innerText;
-  console.log(ratingOnPage);
-  const starPercentage = `${(ratingOnPage / starTotal) * 100}%`;
-  document.querySelector(`.stars-inner`).style.width = starPercentage;
+  const ratingOnPage = document.getElementById('rating').innerText; //1
+  const starPercentage = `${(ratingOnPage / starTotal) * 100}%`; //2
+  document.querySelector(`.stars-inner`).style.width = starPercentage; //3
 };
-
 export { star_rating };
+
+/* how this function works :
+  retrieves the rating:float from the page by looking inside the 'rating' element (1)
+  then makes it a percentage out of 5 (2) and modify the css width property of the hidden
+  yellow stars */

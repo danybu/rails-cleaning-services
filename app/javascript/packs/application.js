fb3b40ee1907cd249f1ecc44411e4e267ec3fe33
bo <bobppamail@gmail.com>
2019-03-08 00:15:55 +0100

    created a js function to get the cleaner in the show banner and did a lil css but nothing much ... to be continued

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,8 +2,9 @@ import "bootstrap";
 import { hoverCard } from './hover-card';
 import { star_rating } from './star_rating';
 import { initMapbox } from './mapboxgl';
+import { cleaner_banner } from './cleaner_banner';
 
 star_rating();
-
 hoverCard();
 initMapbox();
+cleaner_banner();
